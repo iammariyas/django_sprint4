2fc033ea9ee33f2c68cc8f43afe609a386f2ea89
Maria Kruleva <mariakruleva@gmail.com>
2025-12-24 00:56:54 +0400

    Доработка

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -3,40 +3,36 @@ from django.urls import include, path
 from . import views
 from .views import IndexView, PostDetailView, CategoryPostsView
 
-app_name = "blog"
+app_name = 'blog'
 
 post_urls = [
-    path("create/", views.create_post, name="create_post"),
-    path("<int:post_id>/", PostDetailView.as_view(), name="post_detail"),
-    path("<int:post_id>/edit/", views.edit_post, name="edit_post"),
-    path("<int:post_id>/delete/", views.delete_post, name="delete_post"),
-    path("<int:post_id>/comment/", views.add_comment, name="add_comment"),
-    path(
-        "<int:post_id>/edit_comment/<int:comment_id>/",
-        views.edit_comment,
-        name="edit_comment",
-    ),
-    path(
-        "<int:post_id>/delete_comment/<int:comment_id>/",
-        views.delete_comment,
-        name="delete_comment",
-    ),
+    path('create/',
+         views.create_post, name='create_post'),
+    path('<int:post_id>/',
+         PostDetailView.as_view(), name='post_detail'),
+    path('<int:post_id>/edit/',
+         views.edit_post, name='edit_post'),
+    path('<int:post_id>/delete/',
+         views.delete_post, name='delete_post'),
+    path('<int:post_id>/comment/',
+         views.add_comment, name='add_comment'),
+    path('<int:post_id>/edit_comment/<int:comment_id>/',
+         views.edit_comment, name='edit_comment'),
+    path('<int:post_id>/delete_comment/<int:comment_id>/',
+         views.delete_comment, name='delete_comment'),
 ]
 
 profile_urls = [
-    path("edit/", views.edit_profile, name="edit_profile"),
-    path("<str:username>/", views.profile, name="profile"),
+    path('edit/',
+         views.edit_profile, name='edit_profile'),
+    path('<str:username>/',
+         views.profile, name='profile'),
 ]
 
 urlpatterns = [
-    # path('',
-    #      views.index, name='index'),
-    path("", IndexView.as_view(), name="index"),
-    path(
-        "category/<slug:category_slug>/",
-        CategoryPostsView.as_view(),
-        name="category_posts",
-    ),
-    path("posts/", include(post_urls)),
-    path("profile/", include(profile_urls)),
+    path('', IndexView.as_view(), name='index'),
+    path('category/<slug:category_slug>/',
+         CategoryPostsView.as_view(), name='category_posts'),
+    path('posts/', include(post_urls)),
+    path('profile/', include(profile_urls)),
 ]
